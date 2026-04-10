2099256a172367bdb885a7fdfed357c38fb4e7ff
Gesner Deslandes <deslandes78@gmail.com>
2026-04-10 09:36:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from PIL import Image
+import os
 
 st.set_page_config(page_title="My Photo App")
 st.title("My Photo")
 
-try:
-    img = Image.open("my_photo.jpg")
-    st.image(img, caption="This is my photo", width=400)
-except FileNotFoundError:
-    st.error("Photo not found. Make sure the file name matches.")
+# Find the first image file in the current directory
+image_files = [f for f in os.listdir(".") if f.lower().endswith((".jpg", ".jpeg", ".png", ".gif"))]
+
+if image_files:
+    img = Image.open(image_files[0])
+    st.image(img, caption=image_files[0], width=400)
+else:
+    st.error("No image file found. Please upload a .jpg or .png file to the repository.")
